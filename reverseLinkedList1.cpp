e6845e31bc67bf89574a11f6d6f6c300eed857b3
Yashasvi <yashasvi.2024ec1136@kiet.edu>
2022-12-14 14:51:52 +0530

    Create reverseLinkedList1.cpp

---
+++ reverseLinkedList1.cpp
@@ -0,0 +1,19 @@
+Node* reverseLinkedList(Node*head)
+{
+    // Write your code here
+    if(head==NULL || head->next=NULL){
+        return head;
+    }
+    Node* prev=NULL;
+    Node* curr=head;
+    Node* forw=NULL;
+    while(curr!=NULL){
+        forw=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=forw;
+        
+        
+    }
+    return prev;
+}
