70d554f1695400e0654b02d584c754b6b8a24ab2
Yashasvi <princiyyv1302@gmail.com>
2023-04-21 23:34:40 +0530

    Create circularQueue.cpp

---
+++ stack/circularQueue.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h> 
+class CircularQueue{
+    int size;
+    int front;
+    int rear;
+    int *arr;
+    public:
+    // Initialize your data structure.
+    
+
+    CircularQueue(int n){
+        // Write your code here.
+        size=n;
+        front =-1;
+        rear=-1;
+        arr=new int[size];
+
+    }
+
+    // Enqueues 'X' into the queue. Returns true if it gets pushed into the stack, and false otherwise.
+    bool enqueue(int value){
+        // Write your code here.
+        if(front==-1){
+            front=0;
+            rear=0;
+            
+        }
+        else if((front==0 && rear==size-1) || (rear==(front-1)%(size-1))){
+            return false;
+
+        }
+        else if(front!=0 && rear==size-1){
+            rear=0;
+            
+        }
+        else{
+            
+            rear++;
+        }
+        arr[rear]=value;
+
+        return true;
+    }
+
+    // Dequeues top element from queue. Returns -1 if the stack is empty, otherwise returns the popped element.
+    int dequeue(){
+        // Write your code here.
+        if(front==-1){
+            return -1;
+        }
+        int ans=arr[front];
+          arr[front]=-1;
+
+        if(front==rear){
+           front=rear=-1;
+        }
+        else if(front==size-1){
+            front=0;
+        }
+        else{
+            front++;
+        }
+       // arr[rear]=value;
+        return ans;
+    }
+};
