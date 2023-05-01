82f606d39c0d12a7a232c61d5cc7058e66e481c6
Yashasvi <princiyyv1302@gmail.com>
2023-05-01 20:08:11 +0530

    Create asteroidCollision.cpp

---
+++ stack/asteroidCollision.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        
+        stack<int>s;
+        for(int i=0;i<asteroids.size();i++){
+            if(asteroids[i]>0){
+                s.push(asteroids[i]);
+            }
+            else{
+                while(!s.empty() && s.top()>0 && s.top()<abs(asteroids[i])){
+                    s.pop();
+                }
+                if(!s.empty() && s.top()==abs(asteroids[i])){
+                    s.pop();
+                }
+                else if(s.empty() || s.top()<0){
+                    s.push(asteroids[i]);
+                }
+            }
+        }
+        vector<int>ans(s.size());
+        for(int i=(int)s.size()-1;i>=0;i--){
+            ans[i]=s.top();
+            s.pop();
+        }
+      return ans;  
+    }
+};
